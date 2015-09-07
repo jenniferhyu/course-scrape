26dd6dd466411b7d5099a20750125e52af52f179
jenniferhyu <jyu127@berkeley.edu>
2015-09-07 15:41:45 -0700

    We can pickle that

--- ls_scrape.py
+++ ls_scrape.py
@@ -2,22 +2,58 @@ from bs4 import BeautifulSoup, SoupStrainer
 import requests 
 import cPickle as pickle
 
-def setup_soup():
-	url_stem = 'http://guide.berkeley.edu/undergraduate/colleges-schools/letters-science/breadth-requirement-'
-	breadths = ["arts-literature", 
-				"biological-science", 
-				"historical-studies", 
-				"international-studies", 
-				"philosophy-values", 
-				"physical-science", 
-				"social-behavioral-sciences"]
-	url = url_stem + breadths[0]
-	response = requests.get(url).text
-	soup = BeautifulSoup(response, 'lxml', parse_only=SoupStrainer(['tr']))
-	return soup
+class Parser(object):
+	def __init__(self, url_stem="http://guide.berkeley.edu/undergraduate/colleges-schools/letters-science/breadth-requirement-"):
+		self.url_stem = url_stem
 
-def get_course_code(soup):
-	class_codes = [] #massive array!
-	rows = soup.find_all('tr')
-	for item in rows:
-		class_codes.append(item.td.text)
+	def setup_soup(self, url_leaf):
+		url = self.url_stem + url_leaf
+		response = requests.get(url).text
+		soup = BeautifulSoup(response, 'lxml', parse_only=SoupStrainer(['tr']))
+		return soup
+
+class BreadthParser(Parser): # maybe use Parser
+	def __init__(self):
+		self.url_stem = "http://guide.berkeley.edu/undergraduate/colleges-schools/letters-science/breadth-requirement-"
+		self.breadths = ["arts-literature", 
+					"biological-science", 
+					"historical-studies", 
+					"international-studies", 
+					"philosophy-values", 
+					"physical-science", 
+					"social-behavioral-sciences"]
+		self.abbrevs = {"arts-literature":"al",
+					"biological-science":"bs",
+					"historical-studies":"hs", 
+					"international-studies":"is", 
+					"philosophy-values":"pv", 
+					"physical-science":"ps", 
+					"social-behavioral-sciences":"sbs"}
+
+	def make_req_pickle(self, soup, url_leaf, abbrev):
+		# TODO: To convert dept abbrev to full name?
+		file_name = abbrev[url_leaf] + ".pickle"
+		classes = {}
+		rows = soup.find_all('tr')
+		curr_dept = ""
+		stop_counter = 0
+		for item in rows:
+			stop_counter += 1
+			dept, course_num = item.a['title'].split()
+			if dept != curr_dept:
+				classes[dept] = [course_num]
+				curr_dept = dept
+			else:
+				classes[dept].append(course_num)
+		with open(file_name, "wb") as handle:
+			pickle.dump(classes, handle)
+		return classes
+
+def main():	p = Parser()
+	b = BreadthParser()
+	url_leafleaf = b.breadths[0] # will loop later or a better concurrency-related script
+	soup = b.setup_soup(url_leaf)
+	classes = b.make_req_pickle(soup, url_leaf, b.abbrevs)
+
+if __name__ == '__main__':
+	main()
