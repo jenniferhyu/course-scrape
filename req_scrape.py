dfd887da54d26ec9b086b6f70b7eed7877ba18a5
jenniferhyu <jyu127@berkeley.edu>
2015-09-07 17:45:04 -0700

    Foreign language scraped!

--- req_scrape.py
+++ req_scrape.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup, SoupStrainer
 import requests 
 import cPickle as pickle
+# import the pickling/saving module from main codebase
 
 class Parser(object):
-	def __init__(self, url_stem="http://guide.berkeley.edu/undergraduate/colleges-schools/letters-science/breadth-requirement-"):
+	def __init__(self, url_stem):
 		self.url_stem = url_stem
 
 	def setup_soup(self, url_leaf):
@@ -12,7 +13,7 @@ class Parser(object):
 		soup = BeautifulSoup(response, 'lxml', parse_only=SoupStrainer(['tr']))
 		return soup
 
-class BreadthParser(Parser): # maybe use Parser
+class BreadthParser(Parser):
 	def __init__(self):
 		self.url_stem = "http://guide.berkeley.edu/undergraduate/colleges-schools/letters-science/breadth-requirement-"
 		self.breadths = ["arts-literature", 
@@ -36,9 +37,7 @@ class BreadthParser(Parser): # maybe use Parser
 		classes = {}
 		rows = soup.find_all('tr')
 		curr_dept = ""
-		stop_counter = 0
 		for item in rows:
-			stop_counter += 1
 			dept, course_num = item.a['title'].split()
 			if dept != curr_dept:
 				classes[dept] = [course_num]
@@ -49,12 +48,46 @@ class BreadthParser(Parser): # maybe use Parser
 			pickle.dump(classes, handle)
 		return classes
 
+class FLParser(Parser):
+	def __init__(self):
+		pass
+
+	def make_fl_pickle(self, soup):
+		rows = soup.find_all('td')
+		fl_classes = {}
+		changed_dept = ["SCANDIN", "EAEURST", "SLAVIC", "EAEUR ST"]
+		new_dept_abbrev = {"Armenian": "ARMENI",
+						   "Bosnian, Croatian, Serbian": "BOSCRSR",
+						   "Bulgarian": "BULGARI",
+						   "Hungarian","HUNGARI",
+						   "Romanian":"ROMANI",
+						   "Norwegian":"NORWEGN",
+						   }
+		for i in range(1, len(rows), 3):
+			classes = rows[i].text.split()
+			classes = [c.strip(",") for c in classes[1:]]
+			classes = [c.strip(";") for c in classes]
+			dept = classes[0]
+			lang = rows[i].previous_sibling.previous.sibling.text
+			if dept in changed_dept:
+				if lang in new_dept_abbrev:
+					dept = new_dept_abbrev[lang]
+				else:
+					dept = lang.upper()
+			fl_classes[dept] = classes[1:]
+		with open("fl.pickle", "wb") as handle:
+			pickle.dump(fl_classes, handle)
+		return fl_classes
+
 def main():	
-	p = Parser()
+	p_breadth = Parser("http://guide.berkeley.edu/undergraduate/colleges-schools/letters-science/breadth-requirement-")
 	b = BreadthParser()
-	url_leafleaf = b.breadths[0] # will loop later or a better concurrency-related script
+	url_leafleaf = b.breadths[0] # TODO: will loop later or a better concurrency-related script
 	soup = b.setup_soup(url_leaf)
 	classes = b.make_req_pickle(soup, url_leaf, b.abbrevs)
+
+	p_lang = Parser("http://engineering.berkeley.edu/student-services/degree-requirements/foreign-language-courses") #UNTESTED
+
 
 if __name__ == '__main__':
 	main()
