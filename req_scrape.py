7c342292b0e0cd6aee63d7b4d7538bbe4280c0e7
jenniferhyu <jyu127@berkeley.edu>
2015-09-09 21:47:14 -0700

    semicolon bug; hopefully better OOP

--- req_scrape.py
+++ req_scrape.py
@@ -4,8 +4,8 @@ import cPickle as pickle
 # import the pickling/saving module from main codebase
 
 class Parser(object):
-	def __init__(self, url_stem):
-		self.url_stem = url_stem
+	def __init__(self):
+		pass
 
 	def setup_soup(self, url_leaf):
 		url = self.url_stem + url_leaf
@@ -13,27 +13,24 @@ class Parser(object):
 		soup = BeautifulSoup(response, 'lxml', parse_only=SoupStrainer(['tr']))
 		return soup
 
+	def pickler(self, dict_dump, file_name):
+		with open(file_name+".pickle", "wb") as handle:
+			pickle.dump(dict_dump, handle)
+
 class BreadthParser(Parser):
-	def __init__(self):
+	def __init__(self, b):
 		self.url_stem = "http://guide.berkeley.edu/undergraduate/colleges-schools/letters-science/breadth-requirement-"
-		self.breadths = ["arts-literature", 
-					"biological-science", 
-					"historical-studies", 
-					"international-studies", 
-					"philosophy-values", 
-					"physical-science", 
-					"social-behavioral-sciences"]
-		self.abbrevs = {"arts-literature":"al",
-					"biological-science":"bs",
+		self.breadths = {"arts-literature":"al", 
+					"biological-science":"bs", 
 					"historical-studies":"hs", 
 					"international-studies":"is", 
 					"philosophy-values":"pv", 
 					"physical-science":"ps", 
 					"social-behavioral-sciences":"sbs"}
+		self.file_name = self.breadths[b]
 
-	def make_req_pickle(self, soup, url_leaf, abbrev):
+	def make_req_dict(self, soup, url_leaf, abbrev):
 		# TODO: To convert dept abbrev to full name?
-		file_name = abbrev[url_leaf] + ".pickle"
 		classes = {}
 		rows = soup.find_all('tr')
 		curr_dept = ""
@@ -44,30 +41,33 @@ class BreadthParser(Parser):
 				curr_dept = dept
 			else:
 				classes[dept].append(course_num)
-		with open(file_name, "wb") as handle:
-			pickle.dump(classes, handle)
 		return classes
 
 class FLParser(Parser):
 	def __init__(self):
-		pass
+		self.url_stem = "http://engineering.berkeley.edu/student-services/degree-requirements/foreign-language-courses"
 
-	def make_fl_pickle(self, soup):
+	def make_fl_dict(self, soup):
 		rows = soup.find_all('td')
 		fl_classes = {}
 		changed_dept = ["SCANDIN", "EAEURST", "SLAVIC", "EAEUR ST"]
 		new_dept_abbrev = {"Armenian": "ARMENI",
 						   "Bosnian, Croatian, Serbian": "BOSCRSR",
 						   "Bulgarian": "BULGARI",
-						   "Hungarian","HUNGARI",
+						   "Hungarian": "HUNGARI",
 						   "Romanian":"ROMANI",
 						   "Norwegian":"NORWEGN",
 						   }
 		for i in range(1, len(rows), 3):
-			classes = rows[i].text.split(';')
-			classes = ' '.join(classes).split()
-			classes = [c.strip(",") for c in classes[1:]]
-			dept = classes[0]
+			classes = rows[i].text
+			if ';' in classes: 
+				semicolon = classes.index(';')
+				set1, set2 = classes[:semicolon], classes[semicolon+1:]
+				set1 = ''.join(set1.strip(',')).strip().split()
+				set2 = ''.join(set2.strip(',')).strip().split() #move to some kind of tools file
+				dept1, dept2 = set1[0], set2[0]
+				fl_classes[dept1] = set1[1:]
+				fl_classes[dept2] = set2[1:]
 			lang = rows[i].previous_sibling.previous.sibling.text
 			if dept in changed_dept:
 				if lang in new_dept_abbrev:
@@ -75,18 +75,11 @@ class FLParser(Parser):
 				else:
 					dept = lang.upper()
 			fl_classes[dept] = classes[1:]
-		with open("fl.pickle", "wb") as handle:
-			pickle.dump(fl_classes, handle)
-		return fl_classes
+		return fl_classes	
 
 def main():	
-	p_breadth = Parser("http://guide.berkeley.edu/undergraduate/colleges-schools/letters-science/breadth-requirement-")
-	b = BreadthParser()
-	url_leafleaf = b.breadths[0] # TODO: will loop later or a better concurrency-related script
-	soup = b.setup_soup(url_leaf)
-	classes = b.make_req_pickle(soup, url_leaf, b.abbrevs)
-
-	p_lang = Parser("http://engineering.berkeley.edu/student-services/degree-requirements/foreign-language-courses") #UNTESTED
+	pass
+	# TODO: unit tests
 
 
 if __name__ == '__main__':
