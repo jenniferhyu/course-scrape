358721528c12d02307c46d0d0d74e9bec7dbd4e8
jenniferhyu <jyu127@berkeley.edu>
2015-09-11 19:12:47 -0700

    More class attributes

--- req_scrape.py
+++ req_scrape.py
@@ -28,55 +28,64 @@ class BreadthParser(Parser):
 					"physical-science":"ps", 
 					"social-behavioral-sciences":"sbs"}
 		self.file_name = self.breadths[b]
+		self.reqs = {}
 
 	def make_req_dict(self, soup, url_leaf, abbrev):
 		# TODO: To convert dept abbrev to full name?
-		classes = {}
 		rows = soup.find_all('tr')
 		curr_dept = ""
 		for item in rows:
 			dept, course_num = item.a['title'].split()
 			if dept != curr_dept:
-				classes[dept] = [course_num]
+				self.reqs[dept] = [course_num]
 				curr_dept = dept
 			else:
-				classes[dept].append(course_num)
-		return classes
+				self.reqs[dept].append(course_num)
+		return self.reqs
 
 class FLParser(Parser):
 	def __init__(self):
 		self.url_stem = "http://engineering.berkeley.edu/student-services/degree-requirements/foreign-language-courses"
 		self.file_name = "fl"
-
-	def make_fl_dict(self, soup):
-		rows = soup.find_all('td')
-		fl_classes = {}
-		changed_dept = ["SCANDIN", "EAEURST", "SLAVIC", "EAEUR ST"]
-		new_dept_abbrev = {"Armenian": "ARMENI",
+		self.changed_dept = ["SCANDIN", "EAEURST", "SLAVIC", "EAEUR ST"]
+		self.new_dept_abbrev = {"Armenian": "ARMENI",
 						   "Bosnian, Croatian, Serbian": "BOSCRSR",
 						   "Bulgarian": "BULGARI",
 						   "Hungarian": "HUNGARI",
 						   "Romanian":"ROMANI",
 						   "Norwegian":"NORWEGN",
 						   }
+		self.fl_classes = {}
+
+	def make_fl_dict(self, soup):
+		rows = soup.find_all('td')
 		for i in range(1, len(rows), 3):
-			classes = rows[i].text
+			classes = rows[i].text			
 			if ';' in classes: 
+				"""Parses the IRANIAN 110A; PERSIAN 1A, 1B, 11A, 11B, 100A, 100B type cases"""
 				semicolon = classes.index(';')
 				set1, set2 = classes[:semicolon], classes[semicolon+1:]
 				set1 = ''.join(set1.strip(',')).strip().split()
 				set2 = ''.join(set2.strip(',')).strip().split() #move to some kind of tools file
 				dept1, dept2 = set1[0], set2[0]
-				fl_classes[dept1] = set1[1:]
-				fl_classes[dept2] = set2[1:]
-			lang = rows[i].previous_sibling.previous.sibling.text
-			if dept in changed_dept:
-				if lang in new_dept_abbrev:
-					dept = new_dept_abbrev[lang]
-				else:
-					dept = lang.upper()
-			fl_classes[dept] = classes[1:]
+				self.fl_classes[dept1] = set1[1:]
+				self.fl_classes[dept2] = set2[1:]
+			else:
+				classes = classes.split()
+				dept_key = classes[0]
+				codes = ''.join(classes[1:]).split(',')
+				lang = rows[i].previous_sibling.previous.sibling.text
+				if dept_key in self.changed_dept:
+					dept_key = dept_change(dept_key, lang)
+				self.fl_classes[dept_key] = codes
 		return fl_classes	
+
+	def dept_change(self, dept, lang):
+		if lang in self.new_dept_abbrev:
+			dept = self.new_dept_abbrev[lang]
+		else:
+			dept = lang.upper()
+		return dept
 
 def main():	
 	pass
